1f4eb23cd74076c0d627f392e39415f2f3c44393
Sandeep Saini <d.sandeepssb@gmail.com>
2020-12-13 14:15:33 +0530

    Add files via upload

---
+++ precache-manifest.1af02dc46d9e64a2065ced0858556291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cccf550a021d8afd823309ec842363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a8b9004a33593d556c",
+    "url": "/static/css/main.b286ff51.chunk.css"
+  },
+  {
+    "revision": "23fb6285d2a45e88cc65",
+    "url": "/static/js/2.fe430403.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fe430403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a8b9004a33593d556c",
+    "url": "/static/js/main.1d863a41.chunk.js"
+  },
+  {
+    "revision": "0939b0f714c2b1ff39ed",
+    "url": "/static/js/runtime-main.6167e78a.js"
+  }
+]);
